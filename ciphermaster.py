9f302fbc17ce7476fa8523eeae55d073ae868c16
Shaikhul <shaikhuli1@gmail.com>
2023-01-21 22:31:50 +0000

    Added on new lines of code from caesar.py

--- ciphermaster.py
+++ ciphermaster.py
@@ -1,6 +1,6 @@
 # Master script of all the ciphers will be going here, including encryption and decryption
 
-# Simple Substitution #
+### Simple Substitution ###
 import random
 import string
 
@@ -22,7 +22,7 @@ while True:
 
         print(f'Your encryption key is {key}. Please keep it safe!')
         print(f'Your encrypted text: {ciphertext}')
-        
+        break
     elif choice == '2':
 
 # Decryption
@@ -30,6 +30,19 @@ while True:
         dtext = input('Enter the text you want decrypted here: ')
         dkey = input('Enter your encryption key: ')
         alphabet = string.ascii_lowercase
-    else:
-        print('Please enter either 1 or 2.')
+        plaintext = ''
+        for letter in dtext:
+            if letter.lower() in dkey:
+                plaintext += alphabet[dkey.find(letter.lower())]
+            else:
+                plaintext += letter
+        break
+        print(f'Your decrypted text: {plaintext}')
         continue
+
+### Caesar Cipher ###
+
+ptext = input('Enter your text: ')
+shift = input('Enter the shift you want to use. Enter "RANDOM" if you wish to generate a random one: ')
+if shift == 'RANDOM':
+    shift = random.randint(1,25)
