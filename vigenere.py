520f57ecfa5591e34fe92d64896d35d4a00a75d2
Shaikhul <shaikhuli1@gmail.com>
2023-01-30 22:58:30 +0000

    Began encrpytion/decryption and added user input

---
+++ vigenere.py
@@ -0,0 +1,14 @@
+# Vigenere Cipher
+
+def vign_encr():
+    pass
+
+def vign_decr():
+    pass
+
+choice = input('Please press E for encryption, or D for decryption: ')
+
+if choice.upper() == 'E':
+    vign_encr()
+elif choice.upper() == 'D':
+    vign_decr()
