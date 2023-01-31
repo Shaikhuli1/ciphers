af8d77752b710ca0b510f5e7b96dcce4e41b1ea9
Shaikhul <shaikhuli1@gmail.com>
2023-01-31 22:46:20 +0000

    More input text

--- ciphermaster.py
+++ ciphermaster.py
@@ -81,3 +81,20 @@ if choice.upper() == 'E':
     caesar_encr()
 elif choice.upper() == 'D':
     caesar_decr()
+
+### Vigenere Cipher ###
+
+def vign_encr(): 
+    ptext = input('Enter your text: ')
+    shift = input('Enter the shift you want to use. Enter "RANDOM" if you wish to generate a random one: ')
+
+def vign_decr():
+    ctext = input('Enter your text: ')
+    shift = input('Enter the shift you want to use: ')
+
+choice = input('Please press E for encryption, or D for decryption: ')
+
+if choice.upper() == 'E':
+    vign_encr()
+elif choice.upper() == 'D':
+    vign_decr()
