3afbb8fc471679e855bc4eb3416f6f7e9065a501
Shaikhul <shaikhuli1@gmail.com>
2023-01-31 22:44:07 +0000

    More input text

--- vigenere.py
+++ vigenere.py
@@ -1,10 +1,12 @@
 # Vigenere Cipher
 
-def vign_encr():
-    pass
+def vign_encr(): 
+    ptext = input('Enter your text: ')
+    shift = input('Enter the shift you want to use. Enter "RANDOM" if you wish to generate a random one: ')
 
 def vign_decr():
-    pass
+    ctext = input('Enter your text: ')
+    shift = input('Enter the shift you want to use: ')
 
 choice = input('Please press E for encryption, or D for decryption: ')
 
