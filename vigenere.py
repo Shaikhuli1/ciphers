20da3496ac6728ec69d24b2d152c42b64edbf7eb
Shaikhul <shaikhuli1@gmail.com>
2023-02-03 19:58:16 +0000

    Added function to generate key

--- vigenere.py
+++ vigenere.py
@@ -1,8 +1,20 @@
 # Vigenere Cipher
+import string
+alphabet = string.ascii_lowercase
+
+
+def generate_key(ptext,key):
+    keylist = list(key)
+    while len(keylist) < len(ptext):
+        for i in range(len(ptext) - len(keylist)):
+            keylist.append(keylist[i])
+    return ''.join(key)
 
 def vign_encr(): 
-    ptext = input('Enter your text: ')
-    shift = input('Enter the shift you want to use. Enter "RANDOM" if you wish to generate a random one: ')
+    pass
+
+    
+
 
 def vign_decr():
     ctext = input('Enter your text: ')
@@ -11,6 +23,9 @@ def vign_decr():
 choice = input('Please press E for encryption, or D for decryption: ')
 
 if choice.upper() == 'E':
+    ptext = input('Enter your text: ')
+    keyword = input('Enter the keyword you want to use: ')
+    key = generate_key(ptext,keyword)
     vign_encr()
 elif choice.upper() == 'D':
     vign_decr()
