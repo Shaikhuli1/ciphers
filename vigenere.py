f4a15fa0bcee654483ed85db1e4e94033749f193
Shaikhul <shaikhuli1@gmail.com>
2023-02-12 22:22:53 +0000

    Added encryption and driver code

--- vigenere.py
+++ vigenere.py
@@ -10,12 +10,14 @@ def generate_key(ptext,key):
             keylist.append(keylist[i])
     return ''.join(key)
 
-def vign_encr(): 
-    pass
-
+def vign_encr(ptext,key): 
+    ctext = ''
+    for i in range(len(ctext)):
+        x = (ord(ptext[i]) + ord(key[i])) % 26
+        x += ord('A')
+        ctext += chr(x)
+    return ctext
     
-
-
 def vign_decr():
     ctext = input('Enter your text: ')
     shift = input('Enter the shift you want to use: ')
@@ -26,6 +28,7 @@ if choice.upper() == 'E':
     ptext = input('Enter your text: ')
     keyword = input('Enter the keyword you want to use: ')
     key = generate_key(ptext,keyword)
-    vign_encr()
+    ctext = vign_encr(ptext,key)
+    print(f'Cipher text: {ctext}')
 elif choice.upper() == 'D':
     vign_decr()
