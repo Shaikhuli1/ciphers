80e2b5c8fad348a88c8e785b72daa72afd384f4f
Shaikhul <shaikhuli1@gmail.com>
2023-02-14 23:33:35 +0000

    Updated master with updated code

--- ciphermaster.py
+++ ciphermaster.py
@@ -87,22 +87,28 @@ elif choice.upper() == 'D':
 import string
 alphabet = string.ascii_lowercase
 
-def generate_key(ptext,key):
+def generate_key(text,key):
     keylist = list(key)
-    while len(keylist) < len(ptext):
-        for i in range(len(ptext) - len(keylist)):
+    while len(keylist) < len(text):
+        for i in range(len(text) - len(keylist)):
             keylist.append(keylist[i])
     return ''.join(key)
 
-def vign_encr(): 
-    pass
-
+def vign_encr(ptext,key): 
+    ctext = ''
+    for i in range(len(ptext)):
+        x = (ord(ptext[i]) + ord(key[i])) % 26
+        x += ord('A')
+        ctext += chr(x)
+    return ctext
     
-
-
-def vign_decr():
-    ctext = input('Enter your text: ')
-    shift = input('Enter the shift you want to use: ')
+def vign_decr(ctext,key):
+    ptext = ''
+    for i in range(len(ctext)):
+        x = (ord(ctext[i]) - ord(key[i]) + 26) % 26
+        x += ord('A')
+        ptext += chr(x)
+    return ptext
 
 choice = input('Please press E for encryption, or D for decryption: ')
 
@@ -110,6 +116,11 @@ if choice.upper() == 'E':
     ptext = input('Enter your text: ')
     keyword = input('Enter the keyword you want to use: ')
     key = generate_key(ptext,keyword)
-    vign_encr()
+    ctext = vign_encr(ptext,key)
+    print(f'Cipher text: {ctext}')
 elif choice.upper() == 'D':
-    vign_decr()
+    ctext = input('Enter your text: ')
+    keyword = input('Enter the keyword you want to use: ')
+    key = generate_key(ctext,keyword)
+    ptext = vign_decr(ctext,key)
+    print(f'Decrypted text: {ptext}')
