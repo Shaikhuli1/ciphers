0b23f351a01f780083ec68142a3548b00be158f3
Shaikhul <shaikhuli1@gmail.com>
2023-02-23 23:37:07 +0000

    Began updating Caesar with OOP

--- caesar.py
+++ caesar.py
@@ -1,6 +1,6 @@
 # Caesar Cipher
 import random
-
+'''
 def caesar_encr():
     ptext = input('Enter your text: ')
     shift = input('Enter the shift you want to use. Enter "RANDOM" if you wish to generate a random one: ')
@@ -40,3 +40,44 @@ if __name__ == '__main__':
         caesar_encr()
     elif choice.upper() == 'D':
         caesar_decr()
+'''
+class Caesar:
+    def __init__(self):
+        pass
+    
+    def caesar_encr():
+        ptext = input('Enter your text: ')
+        shift = input('Enter the shift you want to use. Enter "RANDOM" if you wish to generate a random one: ')
+        if shift == 'RANDOM':
+            shift = random.randint(1,25)
+
+        ctext = ''
+        for char in ptext:
+            if char.isupper():
+                ctext += chr((ord(char) + shift - 65)% 26 + 65)
+            elif char.islower():
+                ctext += chr((ord(char) + shift - 97)% 26 + 97)
+            else:
+                ctext += char
+
+        print(f'Your shift key is: {shift}. Keep it safe')
+        print(f'Encrypted text: \n{ctext}')
+
+    def caesar_decr():
+        ctext = input('Enter your text: ')
+        shift = input('Enter the shift you want to use: ')
+        ptext = ''
+        for char in ctext:
+            if char.isupper():
+                ptext += chr((ord(char) - shift - 65)% 26 + 65)
+            elif char.islower():
+                ptext += chr((ord(char) - shift - 97)% 26 + 97)
+            else:
+                ptext += char
+
+        print(f'Encrypted text: \n{ptext}')
+
+
+class UserInput:
+    def __init__(self):
+        pass
