16803b9160401bd812592f382343718faf6619b5
Shaikhul <shaikhuli1@gmail.com>
2023-03-08 22:54:59 +0000

    Added driver code

--- onetimepad.py
+++ onetimepad.py
@@ -7,5 +7,15 @@ def decryption(text,key):
     pass
 
 if __name__ == '__main__':
-    encryption()
-    decryption()
+    text = input('Enter your text: ')
+    word = RandomWords()
+
+    while len(word) != len(text):
+        word.get_random_word()
+    
+    choice = input('Please press E for encryption, or D for decryption: ')
+    
+    if choice.upper() == 'E':
+        encryption(text,word)
+    elif choice.upper() == 'D':
+        decryption(text,word)
