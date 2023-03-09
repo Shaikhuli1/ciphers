3b8bd1e6dd32ccc30af9b3ac1debcc836b7cc76f
Shaikhul <shaikhuli1@gmail.com>
2023-03-09 22:53:27 +0000

    Swapped RandomWords for a random string generator

--- onetimepad.py
+++ onetimepad.py
@@ -1,4 +1,14 @@
-from random_word import RandomWords
+#from random_word import RandomWords
+import random
+import string
+
+def get_random_string(length):
+    alphabet = string.ascii_uppercase
+
+    for i in range(length):
+        randstring = ''.join(random.choice(alphabet))
+    return randstring
+
 
 def encryption(text,key):
     pass
@@ -7,15 +17,16 @@ def decryption(text,key):
     pass
 
 if __name__ == '__main__':
-    text = input('Enter your text: ')
-    word = RandomWords()
+    text = input('Enter your text: ').upper().replace(' ','')
+    key = get_random_string(len(text))
+    #word = RandomWords()
 
-    while len(word) != len(text):
-        word.get_random_word()
+    #while len(word) != len(text):
+        #word.get_random_word()
     
     choice = input('Please press E for encryption, or D for decryption: ')
     
     if choice.upper() == 'E':
-        encryption(text,word)
+        encryption(text,key)
     elif choice.upper() == 'D':
-        decryption(text,word)
+        decryption(text,key)
