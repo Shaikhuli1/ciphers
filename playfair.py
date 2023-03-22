2507ddf0eae50a67cee3067bd0a09de8f8d5366b
Shaikhul <shaikhuli1@gmail.com>
2023-03-22 21:50:35 +0000

    Added key generation to refine later

--- playfair.py
+++ playfair.py
@@ -1,6 +1,27 @@
 import random
 import string
 
+def generateKeyTable(word, list1):
+    key_letters = []
+    for i in word:
+        if i not in key_letters:
+            key_letters.append(i)
+ 
+    compElements = []
+    for i in key_letters:
+        if i not in compElements:
+            compElements.append(i)
+    for i in list1:
+        if i not in compElements:
+            compElements.append(i)
+ 
+    matrix = []
+    while compElements != []:
+        matrix.append(compElements[:5])
+        compElements = compElements[5:]
+ 
+    return matrix
+
 def encrypt():
     pass
 
