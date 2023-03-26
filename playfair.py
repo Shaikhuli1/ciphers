85a50a4ad4dc53f165d1f6ad545c9b73fa7b88b6
Shaikhul <shaikhuli1@gmail.com>
2023-03-26 17:50:11 +0100

    Added function that fills in letters, in accordance with playfair rules

--- playfair.py
+++ playfair.py
@@ -11,6 +11,30 @@ def Diagraph(text):
     Diagraph.append(text[group:])
     return Diagraph
 
+def FillerLetter(text):
+    k = len(text)
+    if k % 2 == 0:
+        for i in range(0, k, 2):
+            if text[i] == text[i+1]:
+                new_word = text[0:i+1] + str('x') + text[i+1:]
+                new_word = FillerLetter(new_word)
+                break
+            else:
+                new_word = text
+    else:
+        for i in range(0, k-1, 2):
+            if text[i] == text[i+1]:
+                new_word = text[0:i+1] + str('x') + text[i+1:]
+                new_word = FillerLetter(new_word)
+                break
+            else:
+                new_word = text
+    return new_word
+ 
+ 
+list1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm',
+         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 def generateKeyTable(word, list1):
     key_letters = []
     for i in word:
