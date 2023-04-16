80b0d11404b87e6b6e2c19d30c32186435b002a1
Shaikhul <shaikhuli1@gmail.com>
2023-04-16 18:03:25 +0100

    Rectangle rule added

--- playfair.py
+++ playfair.py
@@ -262,5 +262,9 @@ class Decrypt:
         else:
             self.char2 = matr[e2r+1][e2c]        
 
-    def rectangle_rule(self):
-        pass
+    def rectangle_rule(self,matr, e1r, e1c, e2r, e2c):
+        self.char1 = matr[e1r][e2c]
+    
+        self.char2 = matr[e2r][e1c]
+    
+        return self.char1, self.char2
