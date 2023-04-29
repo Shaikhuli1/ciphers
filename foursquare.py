1ea592aaa9a15d2ab1f4dd1ad8c9177c0c3b7ce8
Shaikhul <shaikhuli1@gmail.com>
2023-04-29 21:04:01 +0100

    Made start on foursquare

---
+++ foursquare.py
@@ -0,0 +1,20 @@
+table = [['A','B','C','D','E'],
+         ['F','G','H','I','J'],
+         ['K','L','M','N','O'],
+         ['P','R','S','T','U'],
+         ['V','W','X','Y','Z']]
+
+def generate_table():
+    pass
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
+
+def position():
+    pass
+
+if __name__ == '__main__':
+    pass
