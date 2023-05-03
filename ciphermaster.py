023c9e62bfd253daebd0088e9ac03a583e40dfdb
Shaikhul <shaikhuli1@gmail.com>
2023-05-03 21:58:14 +0100

    Updated master with foursquare

--- ciphermaster.py
+++ ciphermaster.py
@@ -409,3 +409,25 @@ if __name__ == '__main__':
     
     print("Key text:", key.lower())
     Matrix = generate_table(key, list1)
+
+# Four-Square Cipher #
+table = [['A','B','C','D','E'],
+         ['F','G','H','I','J'],
+         ['K','L','M','N','O'],
+         ['P','R','S','T','U'],
+         ['V','W','X','Y','Z']]
+
+def generate_table():
+    pass
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
+
+def position():
+    pass
+
+if __name__ == '__main__':
+    pass
